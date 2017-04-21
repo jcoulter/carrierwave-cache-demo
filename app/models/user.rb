4f4675c1c3d92744d64f07337518947c72466ab6
Jacob Coulter <jacob.coulter@leandog.com>
2017-04-21 13:58:16 -0400

    Working image cacheing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   require 'carrierwave/orm/activerecord'
 
+  validates :first_name, :last_name, presence: true
+
   mount_uploader :avatar, AvatarUploader, mount_on: 'avatar_name'
 
 end
